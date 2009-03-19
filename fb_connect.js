ffd534c51a189d7b5c72a8f9f9c53f64167673f7
yogadex <yogadex>
2009-03-19 00:02:21 +0000

    numerous cleanup and improvements from 5.x branch

--- fb_connect.js
+++ fb_connect.js
@@ -1,47 +1,42 @@
 
+// Global to keep track of connected state.  Better than facebook's 'reloadIfSessionStateChanged' option.
+var fb_connect_fbu = null;
+
+function fb_connect_on_connected(fbu) {
+    //alert("fb_connect_on_connected " + fbu + " settings fbu is " + Drupal.settings.fb_connect.fbu + " fb_connect_fbu is " + fb_connect_fbu);
+    if (fb_connect_fbu === 0) {
+	// We've gone from not connected to connected.
+	window.location.reload();
+    }
+    fb_connect_fbu = fbu;
+}
+
+function fb_connect_on_not_connected() {
+    //alert("fb_connect_on_not_connected ");
+    if (fb_connect_fbu > 0) {
+	// This code will not be reached if fb_connect_logout_onclick (below) calls logoutAndRedirect.
+	//alert("fb_connect_on_not_connected, reloading... ");
+	// We've gone from connected to not connected.
+	window.location.reload();
+    }
+    fb_connect_fbu = 0;
+}
 
 function fb_connect_login_onclick() {
-    FB.Facebook.get_sessionState().waitUntilReady(function() {
-        var fbu = FB.Facebook.apiClient.get_session() ?
-            FB.Facebook.apiClient.get_session().uid :
-            0;
-	
-	//alert('waitUntilReady callback was called, fbu via javascript is ' + fbu);
-	if (fbu) {
-	    //window.location.reload();
-	}
-    }); 
-    FB.Connect.requireSession();
+    // This will execute before the user fills out the login form.
+    // More important is fb_connect_on_connected, above.
 }
 
 function fb_connect_logout_onclick() {
-    FB.Facebook.get_sessionState().waitUntilReady(function() {
-        var fbu = FB.Facebook.apiClient.get_session() ?
-            FB.Facebook.apiClient.get_session().uid :
-            0;
-	
+    FB.ensureInit(function() {
+	//alert('logout and redirect to ' + Drupal.settings.fb_connect.front_url);
 	FB.Connect.logoutAndRedirect(Drupal.settings.fb_connect.front_url);
     });
-    FB.Connect.requireSession();
 }
 
-function fb_connect_onready() {
-    FB.Facebook.get_sessionState().waitUntilReady(function(session) {
-        var is_loggedin = session ? true : false;
-	
-	var fbu = FB.Facebook.apiClient.get_session() ?
-            FB.Facebook.apiClient.get_session().uid :
-            0;
-	
-	// Sanity check
-	if (fbu != Drupal.settings.fb_connect.fbu) {
-	    // alert('fb_connect_ready... fbu is ' + fbu + ', fb_connect.fbu is ' + Drupal.settings.fb_connect.fbu + ' is_loggedin is ' + is_loggedin);
-	}
-    });
-}
-
+// This function called after fbConnect is initialized
 function fb_connect_init() {
-    if (Drupal.jsEnabled && Drupal.settings.fb_connect.enable_login) {
-	$(document).ready(fb_connect_onready);
-    }
+    // Use show and hide to degrade gracefully when javascript not enabled.
+    $('.fb_connect_show').show();
+    $('.fb_connect_hide').hide();
 }
