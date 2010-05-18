63be11fb5f4e9a0d0a3f4c430ec96053e2eef082
yogadex <yogadex>
2010-05-18 03:08:45 +0000

    now logout of facebook when logging out of drupal

--- fb.js
+++ fb.js
@@ -9,7 +9,7 @@ window.fbAsyncInit = function() {
     settings.cookie = true;
   }
   FB.init(settings);
-  FB.Event.subscribe('auth.sessionChange', FB_JS.sessionChange);
+  FB.Event.subscribe('auth.sessionChange', FB_JS.sessionChange); // Catches login via connect button.
   jQuery.event.trigger('fb_init');  // Trigger event so that other code can now execute.
 };
 
@@ -21,10 +21,38 @@ FB_JS.sessionChange = function(response) {
   jQuery.event.trigger('fb_session_change', status);
 };
 
+FB_JS.initHandler = function() {
+    var session = FB.getLoginStatus(function(response) {
+	var status = {'session' : response.session, 'response': response};
+	if (response.session) {
+	    status.fbu = response.session.uid;
+	    if (Drupal.settings.fb.fbu != status.fbu) {
+		status.changed = true;
+	    }
+	}
+	else if (Drupal.settings.fb.fbu) {
+	    status.changed = true;
+	}
+	if (status.changed) {
+	    jQuery.event.trigger('fb_session_change', status);
+	}
+    });
+
+};
+
 // JQuery pseudo-event handler.
 FB_JS.sessionChangeHandler = function() {
   //debugger;
   window.location.reload();
+};
+
+// click handler
+FB_JS.logoutHandler = function(event) {
+  if (typeof(FB) != 'undefined') {
+    FB.logout(function () {
+      //debugger;
+    });
+  }  
 };
 
 Drupal.behaviors.fb = function(context) {
@@ -38,8 +66,14 @@ Drupal.behaviors.fb = function(context) {
   if (!events || !events.fb_session_change) {
     jQuery(document).bind('fb_session_change', FB_JS.sessionChangeHandler);
   }
-
+  if (!events || !events.fb_init) {
+    jQuery(document).bind('fb_init', FB_JS.initHandler);
+  }
+  
   // Markup with class .fb_show should be visible if javascript is enabled.  .fb_hide should be hidden
   jQuery('.fb_hide', context).hide();
   jQuery('.fb_show', context).show();
+
+  // Logout of facebook when logging out of drupal
+  jQuery("a[href^='" + Drupal.settings.basePath + "logout']", context).click(FB_JS.logoutHandler);
 };
