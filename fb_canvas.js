1cf43003b78748a1ddd10868a7e3de5dfd3748bc
yogadex <yogadex>
2010-08-17 18:57:00 +0000

    control auto-resize in page template.

--- fb_canvas.js
+++ fb_canvas.js
@@ -4,14 +4,17 @@ FB_Canvas = function(){};
 /**
  * Called after Facebook javascript has initialized.  Global FB will be set.
  */
-FB_Canvas.initHandler = function() {
-  // @TODO - call setAutoResize only if iframe is resizeable.
-  FB.Canvas.setAutoResize();
+FB_Canvas.setAutoResize = function() {
+  FB.Canvas.setAutoResize(true, 100); // time in ms, default 100.
 };
 
 /**
  * Enable canvas page specific javascript on this page.
  */
 Drupal.behaviors.fb_canvas = function(context) {
-  jQuery(document).bind('fb_init', FB_Canvas.initHandler);
+  // Resize if body class includes fb_canvas-resizable.
+  $('body.fb_canvas-resizable:not(.fb_canvas-processed)').each(function () {
+    $(this).addClass('fb_canvas-processed');
+    jQuery(document).bind('fb_init', FB_Canvas.setAutoResize);
+  });
 };
