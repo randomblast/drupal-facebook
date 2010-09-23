28f514de41b4bfc24dee5e93b94d439b21643745
yogadex <yogadex>
2010-09-23 15:47:16 +0000

    Restored support for FBML popup dialogs.  Had been broken since 2.x->3.x upgrade.  Now using new FB.ui(...) javascript to support this.

--- fb.js
+++ fb.js
@@ -207,6 +207,83 @@ FB_JS.sessionSanityCheck = function() {
   }
 };
 
+
+//// FBML popup helpers.  Does this code belong here in fb.js?
+
+/**
+ * Move new dialogs to visible part of screen.
+ **/
+FB_JS.centerPopups = function() {
+  var scrollTop = $(window).scrollTop();
+  $('.fb_dialog:not(.fb_popup_centered)').each(function() {
+    var offset = $(this).offset();
+    if (offset.left == 0) {
+      // This is some facebook cruft that cannot be centered.
+    }
+    else if (offset.top < 0) {
+      // Not yet visible, don't center.
+    }
+    else if (offset.top < scrollTop) {
+      $(this).css('top', offset.top + scrollTop + 'px');
+      $(this).addClass('fb_popup_centered'); // Don't move this dialog again.
+    }
+  });
+};
+
+FB_JS.enablePopups = function(context) {
+  // Support for easy fbml popup markup which degrades when javascript not enabled.
+  // Markup is subject to change.  Currently...
+  // <div class=fb_fbml_popup_wrap><a title="POPUP TITLE">LINK MARKUP</a><div class=fb_fbml_popup><fb:SOME FBML>...</fb:SOME FBML></div></div>
+  $('.fb_fbml_popup:not(.fb_fbml_popup-processed)', context).addClass('fb_fbml_popup-processed').prev().each(
+    function() {
+      this.fbml_popup = $(this).next().html();
+      this.fbml_popup_width = parseInt($(this).next().attr('width'));
+      this.fbml_popup_height = parseInt($(this).next().attr('height'));
+      //console.log("stored fbml_popup markup: " + this.fbml_popup); // debug
+      $(this).next().remove(); // Remove FBML so facebook does not expand it.
+    })
+    // Handle clicks on the link element.
+    .bind('click', 
+          function (e) {
+            var popup;
+            //console.log('Clicked!  Will show ' + this.fbml_popup); // debug
+	    
+	    // http://forum.developers.facebook.net/viewtopic.php?pid=243983
+	    var size = FB.UIServer.Methods["fbml.dialog"].size;
+	    if (this.fbml_popup_width) {
+	      size.width=this.fbml_popup_width;
+	    }
+	    if (this.fbml_popup_height) {
+	      size.height=this.fbml_popup_height;
+	    }
+	    FB.UIServer.Methods['fbml.dialog'].size = size;
+	    
+	    // http://forum.developers.facebook.net/viewtopic.php?id=74743
+	    var markup = this.fbml_popup;
+	    if ($(this).attr('title')) {
+	      markup = '<fb:header icon="true" decoration="add_border">' + $(this).attr('title') + '</fb:header>' + this.fbml_popup;
+	    }
+	    var dialog = {
+	      method: 'fbml.dialog', // triple-secret undocumented feature.
+	      display: 'dialog',
+	      fbml: markup,
+	      width: this.fbml_popup_width,
+	      height: this.fbml_popup_height
+	    };
+	    var popup = FB.ui(dialog, function (response) {
+	      console.log(response);
+	    });
+	    
+	    // Start a timer to keep popups centered.
+	    // @TODO - avoid starting timer more than once.
+	    window.setInterval(FB_JS.centerPopups, 500);
+	    
+            e.preventDefault();      
+          })
+    .parent().show();
+};
+
+
 Drupal.behaviors.fb = function(context) {
   // Respond to our jquery pseudo-events
   var events = jQuery(document).data('events');
@@ -221,7 +298,6 @@ Drupal.behaviors.fb = function(context) {
   else {
     $(context).each(function() {
       var elem = $(this).get(0);
-      //alert('fb_connect: ' + elem + $(elem).html()); // debug
       FB.XFBML.parse(elem);
     });
 
@@ -234,5 +310,8 @@ Drupal.behaviors.fb = function(context) {
 
   // Logout of facebook when logging out of drupal
   jQuery("a[href^='" + Drupal.settings.basePath + "logout']", context).click(FB_JS.logoutHandler);
+
+  // Support markup for dialog boxes.
+  FB_JS.enablePopups(context);
 };
 
